0e6512bf8d2b2e923b11d730d88b09f7b5d78132
ma91n <ma91n@users.noreply.github.com>
2026-09-11 05:29:56 +0000

    deploy: 883e89f67955221df33347fd3835c71d02837817

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.Dp8mxV7Y.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.1NKj5Oc5.js";import{_ as a}from"./Mermaid.CSumPBhI.js";import"./chunk-5VM5RSS4.BGwpChqc.js";import"./chunk-XXDRQBXY.tdo68Ni3.js";import"./chunk-KBJHAD2P.CqO9dsZN.js";import"./chunk-2GRJ4B5K.BxTMZqFf.js";import"./channel.DKUPlw5C.js";import"./framework.Bv6G8Ib6.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
